77ae3cf04a0e734369705d75b79f16ad954fed84
mohammad <m.amiri@nizek.com>
2023-04-04 13:58:16 +0330

    add index to elasticsearch log

--- src/Migration/2023_03_26_create_elastic_search_migrations_logs_table.php
+++ src/Migration/2023_03_26_create_elastic_search_migrations_logs_table.php
@@ -6,16 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
 
-    public function up():void
+    public function up(): void
     {
         Schema::create('elastic_search_migrations_logs', function (Blueprint $table) {
             $table->id();
             $table->integer('batch');
+            $table->string('index');
             $table->string('migrations')->index('elastic_search_migrations_logs_migration');
         });
     }
 
-    public function down():void
+    public function down(): void
     {
         Schema::dropIfExists('elastic_search_migrations_logs');
     }
