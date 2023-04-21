977dcd32434a81eb36fbdb1ea0cf7111e05a5c1f
mohammad <mawebcoder@gmail.com>
2023-04-21 10:26:05 +0330

    test testOrWhereGreaterThan

--- tests/Integration/ElasticQueryBuilderIntegrationTest.php
+++ tests/Integration/ElasticQueryBuilderIntegrationTest.php
@@ -972,8 +972,42 @@ class ElasticQueryBuilderIntegrationTest extends TestCase
         $this->assertTrue($results->contains(fn($row) => $row->age === 19));
     }
 
+    /**
+     * @throws FieldNotDefinedInIndexException
+     * @throws RequestException
+     * @throws ReflectionException
+     */
     public function testOrWhereGreaterThan()
     {
+        $data = [
+            'id' => 1,
+            'age' => 19,
+            'name' => 'first',
+            'details' => 'number one'
+        ];
+
+        $data2 = [
+            'id' => 2,
+            'age' => 9,
+            'name' => 'second',
+            'details' => 'number 2'
+        ];
+
+        $this->elastic->create($data);
+
+        $this->elastic->create($data2);
+
+        sleep(2);
+
+        $results = $this->elastic
+            ->where('age', 9)
+            ->orWhere('age', '>', 9)
+            ->get();
+
+        $this->assertEquals(2, $results->count());
+
+        $this->assertTrue($results->contains(fn($row) => $row->age === 19));
+        $this->assertTrue($results->contains(fn($row) => $row->age === 9));
     }
 
     public function testWhereLessThan()
