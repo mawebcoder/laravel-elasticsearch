50a595d27ee5bc2272fd7b56027e6f25cd5e6902
mohammad <m.amiri@nizek.com>
2023-04-30 12:19:21 +0330

    switch default reindex_migration_driver value to sync

--- src/configs/elasticsearch.php
+++ src/configs/elasticsearch.php
@@ -3,6 +3,6 @@
 return [
     'host' => 'http://localhost',
     'port' => 9200,
-    'reindex_migration_driver' => "queue", //sync or queue,
+    'reindex_migration_driver' => "sync", //sync or queue,
     "reindex_migration_queue_name" => 'default'
 ];
